c681dc1b80801ac40e2291320fe98a2c6e6e6c4e
kohsah <ashok@hariharan.org.in>
2018-03-14 11:37:35 +0530

    Updating platform-version to generate download links and show release dates

--- platform-versions.py
+++ platform-versions.py
@@ -28,6 +28,7 @@ HEADER_TEMPLATE = """
     <thead>
         <tr>
             <th style="border: 1px solid black; text-align:center;">Gawati Version</th>
+            <th style="border: 1px solid black; text-align:center;">Rel.Date</th>
             <th style="border: 1px solid black; text-align:center;">Portal UI</th>
             <th style="border: 1px solid black; text-align:center;">Portal Server</th>
             <th style="border: 1px solid black; text-align:center;">Gawati Data</th>
@@ -38,7 +39,8 @@ HEADER_TEMPLATE = """
 
 ROW_TEMPLATE = Template("""
         <tr>
-            <td style="border: 1px solid black; text-align:center;">$version</td>
+            <td style="border: 1px solid black; text-align:center;"><a href="$version_download">$version</a></td>
+            <td style="border: 1px solid black; text-align:center;">$rel_date</td>
             <td style="border: 1px solid black; text-align:center;">$portal_ui</td>
             <td style="border: 1px solid black; text-align:center;">$portal_server</td>
             <td style="border: 1px solid black; text-align:center;">$gawati_data</td>
@@ -50,7 +52,7 @@ ROW_TEMPLATE = Template("""
 VERSION_INFO_HEADER_RST = Template("""
 **Current Version** 
 
-  * GAWATI $version
+  * GAWATI $version  <a href="$version_download">download link</a>, relesased on: $rel_date
 
     """)
 
@@ -66,31 +68,63 @@ VERSION_INFO_FOOTER_RST = """
 
 def files(path):
     for file in os.listdir(path):
-        if (os.path.isfile(os.path.join(path, file))):
+        """ 
+        ignore directories and files starting with __ 
+        """
+        if (os.path.isfile(os.path.join(path, file)) and not file.startswith("__")):
             yield file
 
 def read_kv_file(fileName):
     myvars = {}
-    with open(fileName) as kvfile:
-        for line in kvfile:
-            name, var = line.partition("=")[::2]
-            myvars[name.strip()] = var.strip().replace('"', '')
-    return myvars
+    if os.path.isfile(fileName):
+        with open(fileName) as kvfile:
+            for line in kvfile:
+                name, var = line.partition("=")[::2]
+                myvars[name.strip()] = var.strip().replace('"', '')
+        return myvars
+    else:
+        return myvars
 
 
 def gawati_packages():
     return [file for file in files("versions")]
 
-def versions(vType):
-    return os.listdir(os.path.join("versions", vType))
 
+def versions(vType):
+    version_dirs = os.listdir(os.path.join("versions", vType))
+    return sorted(version_dirs, key=version_sort_func, reverse=True)
+
+"""
+    Used by versions to sort versions in descending order
+"""
+def version_sort_func(key):
+    key_arr = key.split(".")
+    trailing_version = key_arr[2]
+    if (len(trailing_version) == 1 ):
+        trailing_version = '0' + trailing_version
+        key_arr[2] = trailing_version
+        return ".".join(key_arr)
+    else:
+        return key
+
+def version_pkg_bundle_rel_date(packageMode, devVersion):
+    version_meta = read_kv_file(os.path.join("versions", packageMode, devVersion, "__default"))
+    if (version_meta):
+        return version_meta['PkgBundleReleaseDate']
+    else:
+        return 'N/A'
 
 def platform_versions(packageMode): 
+    # packages  ['gawati-data', 'gw-data', 'portal-server', 'portal-ui']
     gwPackages = gawati_packages()
+    # versions  ['1.0.11', '1.0.10', '1.0.9']
     devVersions = versions(packageMode)
     masterVersions = {"name": "gawati", "versions": []}
     for devVersion in devVersions:
         version = { "version" : devVersion, "packages": [] }
+        ## read version date from __default
+        version['rel_date'] = version_pkg_bundle_rel_date(packageMode, devVersion)
+        version['version_download'] = 'http://dl.gawati.org/' + packageMode + "/" + devVersion
         for gwPackage in gwPackages:
             objPackage = read_kv_file(os.path.join("versions", gwPackage))
             objPackageExtended = read_kv_file(
@@ -106,7 +140,9 @@ def platform_versions(packageMode):
         masterVersions["versions"].append(version)
     return masterVersions
 
-
+"""
+pkg_json : the output of platform_versions() which is basically platform-versions.json
+"""
 def generate_table(pkg_json):
     output_table = []
     pkg_versions = pkg_json["versions"]
@@ -116,6 +152,8 @@ def generate_table(pkg_json):
         pkgs = pkg_version['packages']
         pkg_map = {}
         pkg_map["version"] = version
+        pkg_map["rel_date"] = pkg_version['rel_date']
+        pkg_map["version_download"] = pkg_version['version_download']
         for pkg in pkgs:
             #if pkg["version"] == "unrel":
             #    pkg_map[pkg["name"].replace("-", "_")] = "N/A"
@@ -130,7 +168,14 @@ def generate_table(pkg_json):
 
 def version_info_page(this_version):
     output_page = []
-    output_page.append(VERSION_INFO_HEADER_RST.substitute({'version': this_version['version']}))
+    output_page.append(VERSION_INFO_HEADER_RST.substitute(
+        {
+            'version': this_version['version'],
+            'version_download': this_version['version_download'],
+            'rel_date': this_version['rel_date']
+        }
+        )
+    )
     pkgs = this_version["packages"]
     for pkg in pkgs:
         output_page.append(VERSION_INFO_ITEM_RST.substitute({
@@ -142,18 +187,15 @@ def version_info_page(this_version):
     output_page.append(VERSION_INFO_FOOTER_RST)
     return ''.join(output_page)
 
-pkg_versions = platform_versions("dev")
+def main():
 
+    pkg_versions = platform_versions("dev")
+    version_info = version_info_page(pkg_versions['versions'][0])
+    # process Unicode text
+    with io.open('version-compat.rst','w',encoding='utf8') as f:
+        f.write(DOC_TEMPLATE.substitute({"table": generate_table(pkg_versions)}))
+    with io.open('version-info.rst', 'w', encoding='utf8') as f:
+        f.write(version_info)
 
-version_info = version_info_page(pkg_versions['versions'][0])
+main()
 
-#pkg_versions_string = json.dumps(platform_versions("dev"))
-
-#pkg_versions = json.loads(pkg_versions_string)
-
-# process Unicode text
-with io.open('version-compat.rst','w',encoding='utf8') as f:
-    f.write(DOC_TEMPLATE.substitute({"table": generate_table(pkg_versions)}))
-
-with io.open('version-info.rst', 'w', encoding='utf8') as f:
-    f.write(version_info)
